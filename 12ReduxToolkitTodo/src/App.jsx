a0754a0ca9246aaf6f77c9e847a59d9fd63e8e16
Jinansh28 <jinansh2807@gmail.com>
2026-01-17 19:16:44 +0530

    ReduxToolkit

---
+++ 12ReduxToolkitTodo/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+
+function App() {
+
+  return (
+    <>
+      <Todos/>
+      <AddTodo/>
+    </>
+  )
+}
+
+export default App
