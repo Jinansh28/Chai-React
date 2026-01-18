bf1ee65b72dd93c8dc25ad4669b3884e71e85046
Jinansh28 <jinansh2807@gmail.com>
2026-01-18 22:41:29 +0530

    MegaBlog01

---
+++ 13MegaBlog/src/App.jsx
@@ -0,0 +1,14 @@
+import React,{ useState } from 'react'
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
+  return (
+    <>
+      <h1>Blogger</h1>
+    </>
+  )
+}
+
+export default App
