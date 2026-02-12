4df0a6bbba9c5e03f3c86873f1fce15a5ba92c09
Jinansh28 <jinansh2807@gmail.com>
2026-02-12 21:35:04 +0530

    Implement authentication with Redux and add Header/Footer components

--- 13MegaBlog/src/App.jsx
+++ 13MegaBlog/src/App.jsx
@@ -1,14 +1,37 @@
-import React,{ useState } from 'react'
+import React,{ useState, useEffect } from 'react'
+import {useDispatch} from 'react-redux'
+import authservice from "./appwrite/auth.js"
+import { login, logout } from './store/authslice.js'
 import './App.css'
+import { Header, Footer } from './components/index.js'
+import {Outlet} from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  
-  return (
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authservice.getCurrentUser()
+    .then((userData) => {
+      if(userData) dispatch(login({userData}))
+      else dispatch(logout())
+    }) 
+    .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
     <>
-      <h1>Blogger</h1>
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
     </>
-  )
+  ) : null
 }
 
 export default App
